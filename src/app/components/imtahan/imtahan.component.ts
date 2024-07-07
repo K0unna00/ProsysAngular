5c02bf1ebed69c05c0a2eaf9c31c4622dc65506c
K0unna00 <78pf4jz@code.edu.az>
2024-07-07 13:42:48 +0400

    BA

--- src/app/components/imtahan/imtahan.component.ts
+++ src/app/components/imtahan/imtahan.component.ts
@@ -14,12 +14,12 @@ import { ShagirdService } from '../../services/model/shagird/shagird.service';
 })
 export class ImtahanComponent implements OnInit {
   frm: FormGroup;
-  constructor(private shagirdService : ShagirdService ,private dersService : DersService,private imtahanService: ImtahanService, private formBuilder: FormBuilder) {
+  constructor(private shagirdService: ShagirdService, private dersService: DersService, private imtahanService: ImtahanService, private formBuilder: FormBuilder) {
     this.frm = formBuilder.group({
       dersid: ["", Validators.required],
-      shagirdid: ["", Validators.required ],
+      shagirdid: ["", Validators.required],
       tarix: ["", Validators.required],
-      qiymet: ["", [Validators.required, Validators.max(9),Validators.min(0)]]
+      qiymet: ["", [Validators.required, Validators.max(9), Validators.min(0)]]
 
     });
   }
@@ -43,6 +43,10 @@ export class ImtahanComponent implements OnInit {
     return this.frm.get('qiymet')
   }
 
+  get id() {
+    return this.frm.get('id')
+  }
+
   //#endregion
 
   //#region Data
@@ -50,13 +54,14 @@ export class ImtahanComponent implements OnInit {
   public mainData: Imtahan[];
 
   public dersKodData: Ders[];
-  
+
   public shagirdNomreData: Shagird[];
 
   public crudPopupVisible: boolean = false;
 
   listCount: number = 0;
 
+  public crudStatus: boolean;
 
   //#endregion
 
@@ -68,60 +73,109 @@ export class ImtahanComponent implements OnInit {
     this.getShagirdNomre();
   }
 
-  CrudPopupVisibleChange(val: boolean): void {
+  UpdatePopupOpen(imtahan: Imtahan) {
 
+    var date = new Date(imtahan.date);
+
+    console.log(date);
+
+    this.frm = this.formBuilder.group({
+      dersid: [imtahan.lessonId],
+      shagirdid: [imtahan.studentId],
+      tarix: [date],
+      qiymet: [imtahan.grade],
+      id: [imtahan.id]
+    });
+
+    
+    
+
+    this.crudStatus = true;
+
+    this.crudPopupVisible = true;
+  }
+
+  CrudPopupOpen(): void {
     this.frm.reset();
+    this.crudPopupVisible = true;
+  }
 
-    this.crudPopupVisible = val;
+  CrudPopupClose(): void {
+    this.frm.reset();
+    this.crudPopupVisible = false;
   }
 
   getAll() {
     this.imtahanService.getAll().subscribe({
-      next : rs => {
+      next: rs => {
         this.mainData = rs;
       },
       error(err) {
         console.log(err);
-        
+
       },
     });
   }
 
-  getDersKod(){
+  getDersKod() {
     this.dersService.getAll().subscribe(rs => {
       this.dersKodData = rs;
     });
   }
 
-  getShagirdNomre(){
+  getShagirdNomre() {
     this.shagirdService.getAll().subscribe(rs => {
       this.shagirdNomreData = rs;
     });
   }
 
-  create() {
+  CreateUpdate() {
     if (this.frm.valid) {
       const imtahan = new Imtahan();
 
-      imtahan.studentId = this.shagirdid.value,
-      imtahan.date = this.tarix.value,
-      imtahan.grade = parseInt(this.qiymet.value),
-      imtahan.lessonId = this.dersid.value
+      imtahan.studentId = this.shagirdid.value;
+      imtahan.date = this.tarix.value;
+      imtahan.grade = parseInt(this.qiymet.value);
+      imtahan.lessonId = this.dersid.value;
 
-      this.imtahanService.create(imtahan).subscribe({
-        next: data => {
-          console.log(data);
-          this.getAll();
-        },
-        error(err) {
-          console.log(err);
-        },
-      });;
-      this.crudPopupVisible = false;
+      if (this.crudStatus) {
+
+        imtahan.id = this.id.value;
+
+        this.imtahanService.update(imtahan).subscribe({
+          next: data => {
+            this.getAll();
+
+            this.frm.reset();
+
+            this.crudPopupVisible = false;
+          },
+          error(err) {
+            console.log(err);
+          },
+        });
+
+      }
+      else {
+        this.imtahanService.create(imtahan).subscribe({
+          next: data => {
+            this.getAll();
+
+            this.frm.reset();
+
+            this.crudPopupVisible = false;
+          },
+          error(err) {
+            console.log(err);
+          },
+        });
+
+
+      }
     }
   }
 
-  deleteItem(id: string) {
+  DeleteItem(id: string) {
     this.imtahanService.deleteItem(id).subscribe({
       next: result => {
         console.log(result);
